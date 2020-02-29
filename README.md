992e367dafa34fb178fd11f37d3868f8e289c9c4
chriscrichter <chris_richter@outlook.com>
2020-02-29 11:28:03 -0500

    Updated server.js and route files.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Workout-Tracker
 Nosql Homework: Workout Tracker
+
+## Description
+
+This app is a workout tracker. You have already been provided with the front end code in the `Develop` folder. It was built using a Mongo database with a Mongoose schema and handles routes with Express.
+
+
+## Business Context
+
+A consumer will reach their fitness goals quicker when they track their workout progress.
+
+
+## GitHub page
+
+My GitHub page can be found here:
+
+//: chriscrichter GitHub [https://github.com/chriscrichter/](https://github.com/chriscrichter/)
+
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout"
+mongoose.connect(MONGODB_URI, {
+useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+const db = require("./models");
+
+app.use(require("./routes/api-routes.js"));
+app.use(require("./routes/html-routes.js"));
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
+  
