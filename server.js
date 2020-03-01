34ca869c856a589c280b762bf0f201ff312ee6c9
chriscrichter <chris_richter@outlook.com>
2020-03-01 11:00:27 -0500

    Updated server.js file.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
