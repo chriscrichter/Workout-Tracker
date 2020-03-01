503cd66bfb150728728bd647f303c30c30afbebc
chriscrichter <chris_richter@outlook.com>
2020-03-01 08:31:51 -0500

    Committing for Heroku deployment.

--- server.js
+++ server.js
@@ -10,11 +10,17 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+
+mongoose.connect(MONGODB_URI);
+
+/*
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout"
 mongoose.connect(MONGODB_URI, {
 useNewUrlParser: true,
   useFindAndModify: false
 });
+*/
 
 const db = require("./models");
 
