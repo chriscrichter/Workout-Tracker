1137f345968323e83400d6cd4ea325382b335f4a
chriscrichter <chris_richter@outlook.com>
2020-03-01 12:02:41 -0500

    Updated server.js file.

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app.use(express.static("public"));
 
 //"mongodb://localhost/workout"
 //"mongodb://chris:coder2020@ds261238.mlab.com:61238/heroku_cq350zlj"
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://chris:coder2020@ds261238.mlab.com:61238/heroku_cq350zlj"
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://chris:coder2020@ds261238.mlab.com:61238/heroku_cq350zlj";
 mongoose.connect(MONGODB_URI, {
 useNewUrlParser: true,
-  useFindAndModify: true
+  useFindAndModify: false
 });
 
 const db = require("./models");
